9f839958da4e678eae35694748c21b2fd5a2208c
Abdul Ahad <129327562+FrontEndMaestro@users.noreply.github.com>
2024-08-31 19:14:42 +0500

    completed need to chnage color when hovering mouse over a button

---
+++ JS/calculator.js
@@ -0,0 +1,126 @@
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    if(b==0){
+        alert("you naughty little shit");
+        return 0;
+    }
+    return a / b;
+}
+
+function operate(a,op,b) {
+    a=parseFloat(a);
+    b=parseFloat(b);
+    if (op == '+') {
+        return add(a,b);
+    }
+    else if (op == "-") {
+        return subtract(a, b);
+    }
+    else if (op== 'x') {
+        return multiply(a, b);
+    }
+    else if (op == '/') {
+        return divide(a, b);
+    }
+}
+
+// variables to store operand and operators
+let operator="";
+let firstNum="";
+let secondNum="";
+
+console.log(add(4, 1));
+console.log(subtract(4, 1));
+console.log(multiply(4, 2));
+console.log(divide(4, 2));
+
+
+const resultDisplay = document.querySelector('.result');
+const input = document.querySelector('.input');
+const clear = document.querySelector('#clear');
+const numbers = document.querySelectorAll(".button");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+const point=document.querySelector("#point");
+let operatorPressed = false;
+let result;
+
+numbers.forEach(button=>{
+    button.addEventListener("mouseenter",()=>{
+        button.setAttribute("style","background:#B0B0B0");
+    })  
+})
+numbers.forEach(button=>{
+    button.addEventListener("mouseleave",()=>{
+        button.setAttribute("style","background:#E5E4E2");
+    })  
+})
+
+function calculation(){
+    result=operate(firstNum, operator, secondNum);
+    result=result.toFixed(2);
+    firstNum=result.toString();
+    secondNum='';
+    resultDisplay.textContent =result;
+}
+
+equalsButton.addEventListener("click", calculation);
+
+numbers.forEach(btn => {
+    btn.addEventListener('click', () => {
+        input.textContent += btn.textContent;
+        if (operatorPressed == true ) {
+            if(btn.textContent=="."){
+                point.disabled=true;
+            }
+            secondNum+=btn.textContent;
+        }
+        else{
+            if(btn.textContent=="."){
+                point.disabled=true;
+            }
+            firstNum+=btn.textContent;
+        }
+    })
+})
+
+
+operatorButtons.forEach(Operator => {
+    Operator.addEventListener("click", () => {
+        if(operatorPressed==true && firstNum!=null && secondNum!=null && operator!=null){
+                calculation();
+                operator="";
+                operator=Operator.textContent;
+                input.textContent=result+Operator.textContent;
+        }
+        if (operatorPressed == false) {
+            point.disabled=false;
+            operator = Operator.textContent;
+            input.textContent += Operator.textContent;
+            operatorPressed = true;
+        }
+    })
+})
+
+
+//clear functionality
+clear.addEventListener("click", () => {
+    input.textContent = "";
+    resultDisplay.textContent = "";
+    operator = "";
+    firstNum = "";
+    secondNum = "";
+    operatorPressed = false;
+});
+
